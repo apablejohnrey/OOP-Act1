9ce8bf9d200a3dfff55e60153ebdc1249cdd29e4
apablejohnrey <157691011+apablejohnrey@users.noreply.github.com>
2024-01-28 22:16:06 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Activity in OOP</title>
+</head>
+<body>
+    <?php
+
+        $grade =92.1;
+
+        echo "<table border='1' width='500' cellspacing='0'>";
+            echo "<tr bgcolor='lightblue'>";
+                    echo "<td>First Name</td>";
+                    echo "<td>Middle Name</td>";
+                    echo "<td>Last Name</td>";
+                    echo "<td>Age</td>";
+                    echo "<td>Course and Year</td>";
+                    echo "<td>Enrolled: Answer with 'Yes' or 'No'</td>";
+                    echo "<td>Subject</td>";
+                    echo "<td>Grade</td>";
+            echo "</tr>";
+
+            echo "<tr bgcolor='lightgray'>";
+                    echo "<td>John Rey </td>";
+                    echo "<td>Amado</td>";
+                    echo "<td>Apable</td>";
+                    echo "<td>20</td>";
+                    echo "<td>BSIT 2</td>";
+                    echo "<td>Yes</td>";
+                    echo "<td>OOP</td>";
+                    echo "<td>$grade</td>";
+            echo "</tr>";
+
+            echo "<tr bgcolor='white'>";
+                    echo "<td>James Peter</td>";
+                    echo "<td>Ruiz</td>";
+                    echo "<td>Buesing</td>";
+                    echo "<td>20</td>";
+                    echo "<td>BSSW-2</td>";
+                    echo "<td>Yes</td>";
+                    echo "<td>Social Work Counselling</td>";
+                    echo "<td>$grade</td>";
+
+
+            echo "</tr>";
+
+            echo "<tr bgcolor='lightgray'>";
+                    echo "<td>Freya</td>";
+                    echo "<td>Cantoria</td>";
+                    echo "<td>Buesing</td>";
+                    echo "<td>20</td>";
+                    echo "<td>BSCE-2</td>";
+                    echo "<td>Yes</td>";
+                    echo "<td>Basic Calculus</td>";
+                    echo "<td>$grade</td>";
+
+            echo "</tr>";
+                echo "<tr bgcolor='white'>";
+                echo "<td>Christine</td>";
+                echo "<td>B.</td>";
+                echo "<td>Rolda</td>";
+                echo "<td>19</td>";
+                echo "<td>BSIT-2</td>";
+                echo "<td>Yes</td>";
+                echo "<td>INTRO</td>";
+                echo "<td>$grade</td>";
+            echo "</tr>";
+
+            echo "</tr>";
+                echo "<tr bgcolor='lightgray'>";
+                echo "<td>Rea</td>";
+                echo "<td>B.</td>";
+                echo "<td>Buates</td>";
+                echo "<td>20</td>";
+                echo "<td>BSIT-2</td>";
+                echo "<td>Yes</td>";
+                echo "<td>Web Design</td>";
+                echo "<td>$grade</td>";
+            echo "</tr>";
+
+            echo "</tr>";
+                echo "<tr bgcolor='white'>";
+                echo "<td>Angelique</td>";
+                echo "<td>B.</td>";
+                echo "<td>Secretario</td>";
+                echo "<td>20</td>";
+                echo "<td>BSIT-2</td>";
+                echo "<td>Yes</td>";
+                echo "<td>Web Standards</td>";
+                echo "<td>$grade</td>";
+            echo "</tr>";
+
+            echo "</tr>";
+                echo "<tr bgcolor='lightgray'>";
+                echo "<td>Alyanna</td>";
+                echo "<td>A.</td>";
+                echo "<td>Amado</td>";
+                echo "<td>19</td>";
+                echo "<td>BSN-1</td>";
+                echo "<td>Yes</td>";
+                echo "<td>Anaphy</td>";
+                echo "<td>$grade</td>";
+            echo "</tr>";
+
+            echo "</tr>";
+                echo "<tr bgcolor='white'>";
+                echo "<td>Fritzie</td>";
+                echo "<td>B.</td>";
+                echo "<td>Cantoria</td>";
+                echo "<td>19</td>";
+                echo "<td>BSGE-1</td>";
+                echo "<td>Yes</td>";
+                echo "<td>Purposive Communication</td>";
+                echo "<td>$grade</td>";
+            echo "</tr>";
+
+            echo "</tr>";
+                echo "<tr bgcolor='lightgray'>";
+                echo "<td>Rowelyn</td>";
+                echo "<td>B.</td>";
+                echo "<td>Dajuya</td>";
+                echo "<td>19</td>";
+                echo "<td>BSA-1</td>";
+                echo "<td>Yes</td>";
+                echo "<td>Accounting</td>";
+                echo "<td>$grade</td>";
+            echo "</tr>";
+
+            echo "</tr>";
+                echo "<tr bgcolor='white'>";
+                echo "<td>Danielle Joy</td>";
+                echo "<td>P.</td>";
+                echo "<td>Casquejo</td>";
+                echo "<td>19</td>";
+                echo "<td>BSIT-2</td>";
+                echo "<td>No</td>";
+                echo "<td>Digital Design</td>";
+                echo "<td>$grade</td>";
+            echo "</tr>";
+
+        echo "</table>";
+
+    ?>
+</body>
+</html>
